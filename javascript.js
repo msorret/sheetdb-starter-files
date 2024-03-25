e1a9fdd1e1ec2d8d29fed70604df8b33f40e3825
Ms. Orret <orret.deborah@pusd.us>
2024-03-25 23:35:42 +0000

    Starter Files Committed

--- javascript.js
+++ javascript.js
@@ -1,1 +1,27 @@
 
+// USE THIS TO POST THE INFORMATION FROM THE FORM! MAKE SURE YOUR FORM IS ID'D CORRECTLY AND THE INPUT ELEMENTS HAVE THE CORRECT NAME
+// NAME SHOULD BE data[column_name]
+var form = document.getElementById("sheetdb-form");
+form.addEventListener("submit", (e) => {
+    fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.getElementById("sheetdb-form"))
+})
+    .then((response) => response.json())
+    .then((html) => {
+      // you can put any JS code here
+    });
+});
+
+url = ""
+async function get_data () {
+
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    document.getElementById("show_name").innerHTML = data[0].name;
+    document.getElementById("show_msg").innerHTML = data[0].message;
+    
+}
+    
+get_data();
--- python.py
+++ python.py
@@ -1,1 +1,43 @@
+import requests
+
+#PASTE THE API ENDPOINT HERE
+url = ""
+
+# POST THE INFORMATION TO THE API
+
+# DATA NEEDS TO BE CREATED AS AN OBJECT WITH KEYS THAT REFER TO THE COLUMN NAMES
+data_post = {
+}
+
+requests.post(url=url, data=data_post)
+
+
+# GET THE INFORMATION FROM THE API AS A JSON OBJECT
+data_get = requests.get(url).json()
+print(data_get)
+
+
+#EXAMPLE
+url = "https://sheetdb.io/api/v1/3n0u9hhrlo1bp"
+
+# POST THE INFORMATION TO THE API
+
+# DATA NEEDS TO BE CREATED AS AN OBJECT WITH KEYS THAT REFER TO THE COLUMN NAMES
+data_post = {
+}
+
+user_name = input ("What is your name?")
+user_message = input("Enter a message")
+data_post = {
+    'name': user_name,
+    'message': user_message
+}
+requests.post(url=url, data=data_post)
+
+
+# GET THE INFORMATION FROM THE API AS A JSON OBJECT
+data_get = requests.get(url).json()
+print(data_get)
+
+
 
